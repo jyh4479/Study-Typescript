e9de7b79b06bac1b3a10561c6fe3218037568a29
Jeong yong hoon <jyh.dev@gmail.com>
2021-07-02 23:22:03 +0900

    Update TypeTestMain.ts

--- TypeTest/TypeTestMain.ts
+++ TypeTest/TypeTestMain.ts
@@ -1,6 +1,15 @@
-let str = "Hello World"
+// let str = "Hello World"
+//
+// function fun() {
+//     console.log(str)
+// }
+// fun()
 
-function fun() {
-    console.log(str)
-}
-fun(
+let str: string = "Hello"
+let undi: undefined = undefined
+let num: number = 1
+let nul: null = null
+let bool: boolean = true
+// const sym: symbol = Symbol('Development mode')
+
+//타입은 소문자로 사용함 - 타입은 총 6개
